5315a769d48facf1937ced930a3922cedff659ca
Gabriel Leal <gabrielleao.mb@gmail.com>
2020-08-25 14:25:05 -0300

    set the path of assets to relative path

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -14,7 +14,7 @@
 
     <script async src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css">
     <script async src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
@@ -201,7 +201,7 @@
         </div>
     </div>
 </footer>
-<script src="{{ asset('js/app.js') }}"> </script>
+<script src="js/app.js"> </script>
 
 
 <script>
